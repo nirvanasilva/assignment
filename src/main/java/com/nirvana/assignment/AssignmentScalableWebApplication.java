5c3f5968fa6615831e6ae1cfcab39684a2721a85
nirvana.antonio <nirvana.sa@gmail.com>
2020-06-26 17:34:43 -0300

    Add basic project

---
+++ src/main/java/com/nirvana/assignment/AssignmentScalableWebApplication.java
@@ -0,0 +1,13 @@
+package com.nirvana.assignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssignmentScalableWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AssignmentScalableWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nirvana/assignment/AssignmentScalableWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nirvana.assignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssignmentScalableWebApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
